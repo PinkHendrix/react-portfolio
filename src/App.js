ac6621c57461024692f72842f9dd1d565c865207
Rick Hausz <pinkhendrix1881@gmail.com>
2022-09-06 16:32:23 -0700

    Finished the index.js file for Contact page, now on to styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
