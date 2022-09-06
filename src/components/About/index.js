35b5c6c629e0b4e26d153aa873a6ddf1304f679b
Rick Hausz <pinkhendrix1881@gmail.com>
2022-09-05 17:46:32 -0700

    Created About folder within Components folder and created some paragraph info plus started on the 6-sided cube by adding all the font icons of the various tools used.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -31,7 +40,24 @@ const About = () => {
 
       <div className="stage-cube-cont">
         <div className="cube-spinner">
-          <div className="face1"></div>
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
         </div>
       </div>
     </div>
